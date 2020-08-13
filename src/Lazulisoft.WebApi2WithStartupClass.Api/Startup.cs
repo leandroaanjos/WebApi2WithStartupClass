92fc68242fd5778909b71d4b258ecdfb6748b19f
Leandro Dos Anjos <laamaker@gmail.com>
2020-08-12 21:53:58 -0300

    Added method to create db test and connectionstring config

--- src/Lazulisoft.WebApi2WithStartupClass.Api/Startup.cs
+++ src/Lazulisoft.WebApi2WithStartupClass.Api/Startup.cs
@@ -1,7 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http.Formatting;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Lazulisoft.WebApi2WithStartupClass.Api.Data;
 using Lazulisoft.WebApi2WithStartupClass.Api.IoC.Unity;
+using Lazulisoft.WebApi2WithStartupClass.Api.Models;
 using Microsoft.Owin;
 using Newtonsoft.Json.Serialization;
 using Owin;
@@ -33,6 +37,9 @@ namespace Lazulisoft.WebApi2WithStartupClass.Api
             UnityInitializer.Initialize(config);
 
             app.UseWebApi(config);
+
+            // Create the database for test (REMOVE when publish to PROD)
+            InitializeDatabaseAsync(config).GetAwaiter().GetResult();
         }
 
         private void ConfigureWebApi(HttpConfiguration config)
@@ -43,6 +50,28 @@ namespace Lazulisoft.WebApi2WithStartupClass.Api
             // JSON serialize settings
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        private async Task InitializeDatabaseAsync(HttpConfiguration config)
+        {
+            var dbContext = config.DependencyResolver.GetService(typeof(ApplicationDbContext)) as ApplicationDbContext; // new ApplicationDbContext();
+            dbContext.Database.CreateIfNotExists();
+
+            if (dbContext.Database.Exists() && !dbContext.Products.Any())
+            {
+                var products = new List<Product>
+                {
+                    new Product { Name = "XBox One", Price = 299.00M, Category = ProductCategory.VideoGames },
+                    new Product { Name = "PlayStation 4", Price = 320.00M, Category = ProductCategory.VideoGames },
+                    new Product { Name = "Switch", Price = 249.00M, Category = ProductCategory.VideoGames },
+                    new Product { Name = "3DS", Price = 199.00M, Category = ProductCategory.VideoGames },
+                    new Product { Name = "PS Vita", Price = 189.00M, Category = ProductCategory.VideoGames },
+                };
+
+                products.ForEach(x => dbContext.Products.Add(x));
+                await dbContext.SaveChangesAsync();
+
+            }
         }
     }
 }
