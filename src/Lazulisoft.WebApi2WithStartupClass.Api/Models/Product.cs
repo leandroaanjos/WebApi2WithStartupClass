de32b064a7b632e89886658801708fdd611f1097
Leandro Dos Anjos <laamaker@gmail.com>
2020-08-12 21:44:02 -0300

    Added Product models

---
+++ src/Lazulisoft.WebApi2WithStartupClass.Api/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Lazulisoft.WebApi2WithStartupClass.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ProductCategory Category { get; set; }
+    }
+}
---
+++ src/Lazulisoft.WebApi2WithStartupClass.Api/Models/ProductCategory.cs
@@ -0,0 +1,19 @@
+﻿namespace Lazulisoft.WebApi2WithStartupClass.Api.Models
+{
+    public enum ProductCategory
+    {
+        Arts,
+        Automotive,
+        Beauty,
+        Books,
+        Computers,
+        Electronics,
+        Games,
+        Movies,
+        Television,
+        Software,
+        Sports,
+        Toys,
+        VideoGames,
+    }
+}
